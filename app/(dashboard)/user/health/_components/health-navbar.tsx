b6a8cad04976e6472b9b6cade0adb91e01ff49c1
Abraham Samuel {JavaScript Enthusiast} <abmercy035@gmail.com>
2025-04-04 10:42:41 +0100

    Fix/Style
    making navbar responsive and prevent overflowing

--- app/(dashboard)/user/health/_components/health-navbar.tsx
+++ app/(dashboard)/user/health/_components/health-navbar.tsx
@@ -17,21 +17,21 @@ const HousingNavbar = () => {
 
   return (
     <nav className='py-4 bg-[#00BBBB] text-white overflow-x-auto no-scrollbar'>
-      <ul className='container px-4 flex justify-start items-center gap-4 whitespace-nowrap min-w-max'>
+      <ul className='container sm:px-4 px-2 flex justify-start items-center md:gap-4 flex-wrap w-full whitespace-nowrap min-w-max'>
         {links.map((link) => (
           <li key={link.href}>
             <Link
               href={link.href}
               className={classNames(
-                'flex justify-between items-center gap-2 rounded-full font-medium text-[18px] no-underline px-3 py-2 p-2 transition-all duration-300 hover:bg-[hsl(180,65%,57%)]',
+                'flex justify-between items-center gap-2 rounded-full font-medium sm:text-[18px] text-xs no-underline px-3 py-2 p-2 transition-all duration-300 hover:bg-[hsl(180,65%,57%)]',
                 {
                   'bg-[#9eeaea] text-[#4E4B66]': pathname?.startsWith(link.href),
                   'text-white': !pathname?.startsWith(link.href),
                 }
               )}
             >
-              <Icon icon={link.icon} width="24" height="24" stroke={link.stroke} />
-              <span>{link.label}</span>
+              <Icon icon={link.icon} width="24" height="24" stroke={link.stroke} className="h-8 w-8" />
+              <span className="hidden sm:flex">{link.label}</span>
             </Link>
           </li>
         ))}
